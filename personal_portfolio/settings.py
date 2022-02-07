af095d72ff984d70305716d70c33efd832c0a3e1
Matt Stypulkowski <mattstibul@gmail.com>
2022-02-07 14:58:30 +0000

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#o%8wbbyikfdr*_@(f)pi0z!_7qjnwp3h9539@s!0irxojr9!^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://johnwicksdog.pythonanywhere.com/']
 
 
 # Application definition
@@ -129,3 +129,8 @@ MEDIA_URL ='/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production.")
